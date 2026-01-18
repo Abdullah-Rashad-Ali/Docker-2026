c01ef582b96f362a04fec0126b666180996c8916
Abdullah-Rashad-Ali <a201900460@gmail.com>
2026-01-18 00:48:36 +0000

    ingestion

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,152 +1,109 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
+import click
 import pandas as pd
-
-
-# In[5]:
-
-
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
-
-
-# In[6]:
-
-
-url
-
-
-# In[44]:
-
-
-dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
-}
-
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
-
-df = pd.read_csv(
-    url,
-    dtype=dtype,
-    iterator=True,
-    chunksize=100000
-)
-
-
-# In[45]:
-
-
-df.head()
-
-
-# In[40]:
-
-
-df['tpep_pickup_datetime']
-
-
-# In[13]:
-
-
-df['VendorID'].isnull()
-
-
-# In[16]:
-
-
-get_ipython().system('uv add sqlAlchemy psycopg2-binary')
-
-
-# In[34]:
-
-
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
 
+@click.command()
+@click.option(
+    '--url',
+    default='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz',
+    help='CSV file URL'
+)
+@click.option(
+    '--user',
+    default='root',
+    help='Database user'
+)
+@click.option(
+    '--password',
+    default='root',
+    help='Database password'
+)
+@click.option(
+    '--host',
+    default='localhost',
+    help='Database host'
+)
+@click.option(
+    '--port',
+    default=5432,
+    type=int,
+    help='Database port'
+)
+@click.option(
+    '--db',
+    default='ny_taxi',
+    help='Database name'
+)
+@click.option(
+    '--table',
+    default='yellow_taxi_data',
+    help='Target table name'
+)
+@click.option(
+    '--chunksize',
+    default=100000,
+    type=int,
+    help='Number of rows per chunk'
+)
+def main(url, user, password, host, port, db, table, chunksize):
+    """Ingest NYC taxi data into PostgreSQL database."""
+    
+    # Construct database URL from parameters
+    db_url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
+    
+    dtype = {
+        "VendorID": "Int64",
+        "passenger_count": "Int64",
+        "trip_distance": "float64",
+        "RatecodeID": "Int64",
+        "store_and_fwd_flag": "string",
+        "PULocationID": "Int64",
+        "DOLocationID": "Int64",
+        "payment_type": "Int64",
+        "fare_amount": "float64",
+        "extra": "float64",
+        "mta_tax": "float64",
+        "tip_amount": "float64",
+        "tolls_amount": "float64",
+        "improvement_surcharge": "float64",
+        "total_amount": "float64",
+        "congestion_surcharge": "float64"
+    }
 
+    parse_dates = [
+        "tpep_pickup_datetime",
+        "tpep_dropoff_datetime"
+    ]
 
-# In[35]:
-
-
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[36]:
-
-
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[42]:
-
-
-for df_chunk in df:
-    print(len(df_chunk))
-
-
-# In[47]:
-
-
-df.to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[48]:
-
-
-first = True
-
-for df_chunk in df:
-
-    if first:
-        # Create table schema (no data)
-        df_chunk.head(0).to_sql(
-            name="yellow_taxi_data",
-            con=engine,
-            if_exists="replace"
-        )
-        first = False
-        print("Table created")
-
-    # Insert chunk
-    df_chunk.to_sql(
-        name="yellow_taxi_data",
-        con=engine,
-        if_exists="append"
+    df = pd.read_csv(
+        url,
+        dtype=dtype,
+        iterator=True,
+        chunksize=chunksize
     )
 
-    print("Inserted:", len(df_chunk))
+    engine = create_engine(db_url)
+    first = True
 
+    for df_chunk in df:
+        if first:
+            df_chunk.head(0).to_sql(
+                name=table,
+                con=engine,
+                if_exists="replace"
+            )
+            first = False
+            print("Table created")
 
-# In[55]:
+        df_chunk.to_sql(
+            name=table,
+            con=engine,
+            if_exists="append"
+        )
+        print("Inserted:", len(df_chunk))
 
-
-sql = 'select count(*) from ny_taxi.yellow_taxi_date'
-pd.read_sql_query(sql,con=engine)
-
-
-# In[ ]:
+if __name__ == '__main__':
+    main()
 
 
 
